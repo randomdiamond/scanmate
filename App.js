aa738ca89e1d29bb4219b9f8e4f58f4d4086814c
randomdiamond <97394586+randomdiamond@users.noreply.github.com>
2023-03-05 00:02:44 +0100

    version 1.0

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import CameraPermissions from './CameraPermissions.js';
+import Scanner from './Scanner.js';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false)
+
+
+
+  async function askForCameraPermission() {
+    const { status } = await BarCodeScanner.requestPermissionsAsync()
+    console.log(status)
+
+    setHasPermission(status === 'granted')
+  }
+  useEffect(() => {
+    askForCameraPermission()
+  }, [])
+
+
+
+  // Check permissions and return the screen
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {!hasPermission && <CameraPermissions askForCameraPermission={askForCameraPermission} />}
+      {hasPermission && <Scanner />}
+
+    </>
   );
 }
 
